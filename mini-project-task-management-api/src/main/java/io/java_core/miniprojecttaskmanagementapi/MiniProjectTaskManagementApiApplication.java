1e5cb588ce3ba56b22657d4f3ac9ae2aac158c5e
Ketan <ketan_r598@outlook.com>
2026-07-14 12:09:13 +0530

    Mini project: Spring Boot Mini Project Initial Commit

---
+++ mini-project-task-management-api/src/main/java/io/java_core/miniprojecttaskmanagementapi/MiniProjectTaskManagementApiApplication.java
@@ -0,0 +1,13 @@
+package io.java_core.miniprojecttaskmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectTaskManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectTaskManagementApiApplication.class, args);
+    }
+
+}
---
+++ mini-project-task-management-api/src/test/java/io/java_core/miniprojecttaskmanagementapi/MiniProjectTaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java_core.miniprojecttaskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectTaskManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
